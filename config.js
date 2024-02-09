64b0e69553cd686a4789061cdb8b5c11084aa095
Darrenmcbright <159401231+Darrenmcbright@users.noreply.github.com>
2024-02-09 15:22:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['+260 770558635', 'Darren', true],
+  ['+260 770558635', 'Ash', true], 
   [''] 
 ] //Number of owners
 
